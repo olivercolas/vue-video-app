76bc797e6b5fca4563ff21033d33b83917ad90ca
olivercolas <olivercolas.oc@gmail.com>
2020-06-28 15:28:48 +0300

    chore: removed base ui injected by vuetify & vue-cli

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex align-center">
+    <router-link to="/video/randomId">Go to a video</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
