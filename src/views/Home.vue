13b8e3324d04cada916abb332328d2434f476104
olivercolas <olivercolas.oc@gmail.com>
2020-06-28 20:12:24 +0300

    feat: added carousel for videos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="home d-flex align-center">
-    <router-link to="/video/randomId">Go to a video</router-link>
+  <div class="home">
+    <v-carousel v-model="model" class="carousel" height="600">
+      <v-carousel-item v-for="movie in movies" :key="movie.id">
+        <v-img
+          height="100%"
+          v-ripple
+          @click="selectVideo(movie.id)"
+          class="carousel__img"
+          :src="movie.poster"
+        >
+        </v-img>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home',
-  components: {}
-};
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import { State } from 'vuex-class';
+import { Movie, MovieState } from '../store/modules/movies/models';
+
+@Component
+export default class Home extends Vue {
+  @State((state: { movies: MovieState }) => state.movies.list) movies!: Movie[];
+
+  model = 0;
+
+  selectVideo(id: number) {
+    this.$router.push({ name: 'Video', params: { id: id.toString() } });
+  }
+}
 </script>
+
+<style lang="scss">
+.carousel {
+  margin: 40px;
+  width: calc(100% - 80px) !important;
+
+  &__img {
+    cursor: pointer;
+  }
+}
+</style>
